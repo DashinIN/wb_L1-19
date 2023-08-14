b15a008fd572eed7d031aa6a486bbb7925ba2543
DashinIN <green19grant@yandex.ru>
2023-08-14 16:40:45 +0300

    who is rederecting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // https://oauth.vk.com/authorize?client_id=51728435&display=page&redirect_uri=https://dashinin.github.io/wb_L1-19&scope=wall&response_type=token&v=5.131&state=123456
 
 // window.location.href = 'https://oauth.vk.com/authorize?client_id=51728435&display=page&redirect_uri=https://dashinin.github.io/wb_L1-19&scope=wall&response_type=token&v=5.131&state=123456'
-
+//ww
 console.log(window.location.hash)
 
 const wrapper = document.querySelector('.wrapper')
